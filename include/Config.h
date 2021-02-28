897bb54d8eebbe11b9caefb3aeaa54fa18b5b162
Samuel <Samuel@oosterholt.com>
2021-02-28 23:23:48 +0100

    Added WiFiConfig.h

--- include/Config.h
+++ include/Config.h
@@ -7,6 +7,6 @@
 #define SEGMENT_COUNT 1       // Amount of (sub)segments
 #define SEGMENT_SIZE 120       // Amount of leds in each segment by default
 #define LED_PIN 4             // Pin output to the led cube
-#define BRIGHTNESS 125        // General Brightness of the leds
+#define BRIGHTNESS 255        // General Brightness of the leds
 #define LED_TYPE WS2812B       // Led strip used in cube
 #define COLOR_ORDER GRB       // Default color ordering
---
+++ lib/WiFiConfig.h
@@ -0,0 +1,11 @@
+//
+// Created by Samuel on 28/02/2021.
+//
+
+#ifndef INFINITY_CUBE_WIFICONFIG_H
+#define INFINITY_CUBE_WIFICONFIG_H
+
+const char *SSID = "";
+const char *WiFiPassword = "";
+
+#endif //INFINITY_CUBE_WIFICONFIG_H
